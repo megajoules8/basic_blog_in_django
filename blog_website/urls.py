bfb35e34994a905b12e7c5c5ebae22c4176b6e45
Mykalin Jones <mykalin.jones@gmail.com>
2026-03-03 13:29:29 +0100

    Modified files to make it a basic template project

--- blog_website/urls.py
+++ blog_website/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Ellens_website project.
+URL configuration for blog_website project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
